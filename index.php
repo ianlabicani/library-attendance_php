2a51412adf5827d47e13d47beb9b7c6169678766
ianlabicani <labicaniian@gmail.com>
2023-10-04 12:04:34 +0800

    10-04-2023

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Library Attendance</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js
+" defer></script>
 </head>
 
 <body>
@@ -17,60 +20,74 @@
     <title>CICS Attendance</title>
 
     <style>
-      #signature {
-        border: 1px solid black;
-      }
+    #signature {
+      border: 1px solid black;
+    }
     </style>
   </head>
 
   <body>
     <h2>Attendance Form</h2>
-    <form action="process.php" method="post">
-      <label for="lastname">Last Name:</label>
-      <input type="text" id="lastname" name="lastname" required><br><br>
+    <div class="container mt-5">
+      <form action="process.php" method="post">
+        <div class="form-group">
+          <label for="lastname">Last Name:</label>
+          <input type="text" class="form-control" id="lastname" name="lastname" required>
+        </div>
 
-      <label for="firstname">First Name:</label>
-      <input type="text" id="firstname" name="firstname" required><br><br>
+        <div class="form-group">
+          <label for="firstname">First Name:</label>
+          <input type="text" class="form-control" id="firstname" name="firstname" required>
+        </div>
 
-      <label for="middlename">Middle Name:</label>
-      <input type="text" id="middlename" name="middlename"><br><br>
-      <label for="student_number">Student Number:</label>
-      <input type="text" id="student_number" name="student_number" required><br><br>
-      <label for="course">Course:</label>
-      <select name="course" id="course">
-        <option value="bsit">bsit</option>
-        <option value="crim">crim</option>
-        <option value="chm">chm</option>
-        <option value="educ">educ</option>
-        <option value="cit">cit</option>
-        <option value="cfas">cfas</option>
-        <option value="cbea">cbea</option>
-      </select>
-      <BR></BR>
+        <div class="form-group">
+          <label for="middlename">Middle Name:</label>
+          <input type="text" class="form-control" id="middlename" name="middlename">
+        </div>
 
-      <label for="year_level">Year Level:</label>
-      <select name="year_level" id="year-level">
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-      </select>
-      <br>
-      <label for="section">Section:</label>
-      <select name="section" id="section">
-        <option value="a">a</option>
-        <option value="b">b</option>
-        <option value="c">c</option>
-        <option value="d">d</option>
-        <option value="e">e</option>
-        <option value="f">f</option>
-      </select>
-      <br>
+        <div class="form-group">
+          <label for="student_number">Student Number:</label>
+          <input type="text" class="form-control" id="student_number" name="student_number" required>
+        </div>
 
+        <div class="form-group">
+          <label for="course">Course:</label>
+          <select class="form-control" id="course" name="course">
+            <option value="bsit">bsit</option>
+            <option value="crim">crim</option>
+            <option value="chm">chm</option>
+            <option value="educ">educ</option>
+            <option value="cit">cit</option>
+            <option value="cfas">cfas</option>
+            <option value="cbea">cbea</option>
+          </select>
+        </div>
 
-      <input type="submit" value="Submit">
-    </form>
-    <br><br><br>
+        <div class="form-group">
+          <label for="year_level">Year Level:</label>
+          <select class="form-control" id="year-level" name="year_level">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+          </select>
+        </div>
+
+        <div class="form-group">
+          <label for="section">Section:</label>
+          <select class="form-control" id="section" name="section">
+            <option value="a">a</option>
+            <option value="b">b</option>
+            <option value="c">c</option>
+            <option value="d">d</option>
+            <option value="e">e</option>
+            <option value="f">f</option>
+          </select>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
     <hr>
 
   </body>
@@ -87,7 +104,7 @@
 
 
   // Create an SQL query to select data from the database
-  $sql = "SELECT * FROM students_tbl";
+  $sql = "SELECT * FROM students_tbl ORDER BY id DESC";
 
   // Execute the query and store the result
   $result = $conn->query($sql);
@@ -95,48 +112,51 @@
   // Check if there are any records
   if ($result->num_rows > 0) {
     echo "<h2>Attendance Records</h2>";
-    echo "<table border='1'>";
-    echo "<tr>
-    <th>Last Name</th>
-    <th>First Name</th>
-    <th>Middle Name</th>
-    <th>Student Number</th>
-    <th>course</th>
-    <th>Year Level</th>
-    <th>Section</th>
-    <th>time in</th>
-    <th>time out</th>
-  </tr>";
+    echo "<table class='table table-bordered'>";
+    echo "<thead class='thead-dark'>
+          <tr>
+              <th>Last Name</th>
+              <th>First Name</th>
+              <th>Middle Name</th>
+              <th>Student Number</th>
+              <th>Course</th>
+              <th>Year Level</th>
+              <th>Section</th>
+              <th>Time In</th>
+              <th>Time Out</th>
+          </tr>
+      </thead>";
+    echo "<tbody>";
 
     // Loop through the records and display them in a table
     while ($row = $result->fetch_assoc()) {
       echo "<tr>";
       echo "<form action='update.php' method='post'>";
-      echo "<input type='hidden' name='student_id' value='${row['id']}'>";
-      echo "<td>" . $row["lastname"] . "</td>";
-      echo "<td>" . $row["firstname"] . "</td>";
-      echo "<td>" . $row["middlename"] . "</td>";
-      echo "<td>" . $row["student_number"] . "</td>";
-      echo "<td>" . $row["course"] . "</td>";
-      echo "<td>" . $row["year_level"] . "</td>";
-      echo "<td>" . $row["section"] . "</td>";
-      echo "<td>" . $row["time_in"] . "</td>";
-      echo "<td>" . $row["time_out"] . "</td>";
-      if (!$row["time_in"]) {
-        echo "<td><button name='input_time' value='input_time'>time in</button></td>";
-        echo "<td ><button disabled>time out</button></td>";
-      } else {
-        echo "<td ><button disabled>time in</button></td>";
-        echo "<td ><button name='input_time' value='input_time'>time out</button></td>";
+      echo "<input type='hidden' name='student_id' value='{$row['id']}'>";
+
+      $columns = ["lastname", "firstname", "middlename", "student_number", "course", "year_level", "section", "time_in", "time_out"];
+
+      foreach ($columns as $column) {
+        echo "<td>{$row[$column]}</td>";
       }
-      echo "</>";
+
+      $time_in_button = !$row["time_in"] ? "<button class='btn btn-primary' name='time_in' value='time_in'>Time In</button>" : "<button class='btn btn-secondary' disabled>Time In</button>";
+      $time_out_button =  $row["time_out"] ? "<button class='btn btn-secondary' disabled>Time Out</button>" : "<button class='btn btn-primary' name='time_out' value='time_out'>Time Out</button>";
+
+      echo "<td>$time_in_button</td>";
+      echo "<td>$time_out_button</td>";
+
+      echo "</form>";
       echo "</tr>";
     }
 
+    echo "</tbody>";
     echo "</table>";
   } else {
     echo "No attendance records found.";
   }
+  ?>
+
 
   // Close the database connection
   $conn->close();
--- update.php
+++ update.php
@@ -5,7 +5,7 @@ date_default_timezone_set('Asia/Manila');
 // Get and display the current time in the Philippines timezone
 $current_time = date("Y-m-d H:i:s"); // You can adjust the format as needed
 
-if (isset($_POST['input_time'])) {
+if (isset($_POST['time_in'])) {
   $student_id = $_POST['student_id']; // Replace with your method of obtaining the student ID
   $time_in = date("Y-m-d H:i:s"); // Get the current timestamp
 
@@ -27,4 +27,23 @@ if (isset($_POST['input_time'])) {
 
   // Handle the button click event here
   // For example, update database records or perform any desired action.
+} else if (isset($_POST['time_out'])) {
+  $student_id = $_POST['student_id']; // Replace with your method of obtaining the student ID
+  $time_out = date("Y-m-d H:i:s"); // Get the current timestamp
+
+  // Prepare and execute the SQL update statement
+
+  $sql = "UPDATE students_tbl SET time_out = ? WHERE id = ?";
+  // $sql1 = "UPDATE students_tbl SET time_in = ? WHERE id = ?";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param("ss", $time_out, $student_id); // Assuming student_number is the correct column name
+
+  if ($stmt->execute()) {
+    // $sql1 = "Select * from students_tbl where id = $student_id";
+    echo "";
+    echo "Time-out updated successfully.";
+    header("Location: index.php");
+  } else {
+    echo "Error updating time-in: " . $stmt->error;
+  }
 }
